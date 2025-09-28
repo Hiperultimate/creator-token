35e83eb4dce9f1e20d1d22b32ef2df8883a8c959
hiper <hiperultimatevegito@gmail.com>
2025-09-28 15:18:10 +0530

    Moving anchor and adding base express backend template

---
+++ backend/index.ts
@@ -0,0 +1,13 @@
+// src/app.ts
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express with TypeScript!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
